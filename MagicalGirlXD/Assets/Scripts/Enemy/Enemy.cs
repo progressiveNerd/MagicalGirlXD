85fd5b9b36cb6bccb33894b3953a43357e19c446
Cody Black <cody.black.47@gmail.com>
2015-11-24 11:17:35 -0700

    Revert "implemented animations for player and enemy"
    This reverts commit 601fed5593eaac7d01981158ff282979fcb4397a.

--- MagicalGirlXD/Assets/Scripts/Enemy/Enemy.cs
+++ MagicalGirlXD/Assets/Scripts/Enemy/Enemy.cs
@@ -162,7 +162,6 @@ public class Enemy : Entity {
             }
         }
         directionIndicator.SetDirection(direction);
-        //Animating();
     }
 
     public void OnChildTriggerEnter(string aName, Collider2D aOther) {
@@ -245,10 +244,5 @@ public class Enemy : Entity {
 
     public void DestinationReached() {
         destinationReached = true;
-    }
-
-    void Animating()
-    {
-        anim.SetInteger("Direction", (int)direction);
     }
 }
